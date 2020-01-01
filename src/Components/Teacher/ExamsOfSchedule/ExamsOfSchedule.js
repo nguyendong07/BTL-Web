34fb6b42fb2d8de4711af2af7ce3fa6ec190d87c
DuyBuiTien <43126830+DuyBuiTien@users.noreply.github.com>
2020-01-02 01:43:10 +0700

    Add Schedule

--- src/Components/Teacher/ExamsOfSchedule/ExamsOfSchedule.js
+++ src/Components/Teacher/ExamsOfSchedule/ExamsOfSchedule.js
@@ -5,19 +5,26 @@ import Axios from 'axios';
 import { URL_GET_EXAMS_OF_SCHEDULE } from '../../../Config/Api';
 import './ExamsOfSchedule.css';
 export default function (props) {
-  const [exams, setExams] = useState([]);
+  const [exams, setExams] = useState([])
+
+
   useEffect(() => {
-    getExams();
+    getExams()
   }, [])
 
   function getExams() {
     const scheduleID = props.match.params.scheduleID;
     const url = `${URL_GET_EXAMS_OF_SCHEDULE}/${scheduleID}`
     Axios.get(url).then(rs => {
-      console.log(rs.data);
+      // console.log(rs.data);
       setExams(rs.data);
     })
   }
+
+ 
+
+
+
   return (
     <React.Fragment>
       <HeaderElement></HeaderElement>
@@ -25,20 +32,23 @@ export default function (props) {
         style={{ float: "left" }}>
         >
         <MenuTeacher></MenuTeacher>
-        </div>
-        <div id="frame-examsofshedule"
-          style={{ float: 'right', marginTop: "20vh" }}
-        >
-          {exams.map(item => {
-            return (
-              <div id="frameSchedule">
-                <p style={{ fontSize: "18px" }}> Môn thi: {item.courseName} - {item.courseChar}<br /><br />
-                  Thời gian thi từ: {item.startTime} đến {item.endTime} Phòng : {item.room}<br />
-                </p>
-              </div>
-            )
-          })}
-        </div>
+      </div>
+      <div id="frame-examsofshedule"
+        style={{ float: 'right', marginTop: "20vh" }}
+      >
+        
+        {exams.map(item => {
+          return (
+            <div id="frameSchedule">
+              <p style={{ fontSize: "18px" }}> Môn thi: {item.courseName} - {item.courseChar} </p><br /><br />
+              <p style={{ fontSize: '16px', fontStyle: 'italic' }}>  Thời gian thi từ: {item.startTime} - {item.endTime}</p>
+              <p style={{ float: 'right', position: 'relative', bottom: '40px', right: '10px', fontSize: '16px', fontStyle: 'italic' }}> Phòng : {item.room}<br />
+              </p>
+            </div>
+          )
+        })}
+        
+      </div>
     </React.Fragment>
   )
 }
